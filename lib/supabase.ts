b716d6837ed058c4ba331b5225807b091ce2e65f
nkropa96 <32389903+nkropa96@users.noreply.github.com>
2025-10-12 13:17:28 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://sitycikfywovzeoatwgi.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpdHljaWtmeXdvdnplb2F0d2dpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyMDYzMDIsImV4cCI6MjA3NTc4MjMwMn0.ob4qD0ihhAUVEkbeclfte_6HjIma1gRg_5-ATBoqPsY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
